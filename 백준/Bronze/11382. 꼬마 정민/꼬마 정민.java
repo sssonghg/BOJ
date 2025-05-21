0129b88b7fd41e8611b5d6775d78f585da72a00b
sssonghg <136361200+sssonghg@users.noreply.github.com>
2025-05-21 13:59:41 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        long num1 = input.nextLong();
+        long num2 = input.nextLong();
+        long num3 = input.nextLong();
+
+        System.out.println(num1+num2+num3);
+    }
+}
